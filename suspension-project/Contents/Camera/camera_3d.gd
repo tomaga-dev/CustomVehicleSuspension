56155bb373dbc684ee4da8ad76d3c01f471502ac
tomaga-dev <tomaga1080@protonmail.com>
2026-06-14 15:12:56 +0200

    You can now switch between third-person and first-person view
    You use the "C"-key to switch the camera.

--- suspension-project/Contents/Camera/camera_3d.gd
+++ suspension-project/Contents/Camera/camera_3d.gd
@@ -1,17 +1,34 @@
 extends Camera3D
 
-var target: VehicleController
+@export var first_person: bool = false
 
-var distance_measured: Vector3
-var distance_magnitude: float
+var target: VehicleController
+var camera_basis: Basis
+var camera_offset: Vector3
 
 func setup(car:VehicleController) -> void:
 	target = car
-	distance_measured = target.global_transform.origin - global_transform.origin
-	distance_magnitude = distance_measured.length()
+	var distance_measured: Vector3 = target.global_transform.origin - global_transform.origin
+	var distance_magnitude: float = distance_measured.length()
+	camera_basis = transform.basis
+	camera_offset = camera_basis * Vector3.FORWARD * distance_magnitude
 
 func _physics_process(_delta: float) -> void:
 	if !target:
 		return
-	var r = Quaternion(transform.basis)
-	transform.origin = target.global_transform.origin - r * Vector3.FORWARD * distance_magnitude
+	if Input.is_action_just_pressed("camera"):
+		first_person = !first_person
+	if first_person:
+		target.visible = false
+		first_person_view()
+	else:
+		target.visible = true
+		fixed_angle_view()
+
+func fixed_angle_view() -> void:
+	transform.basis = camera_basis
+	transform.origin = target.global_transform.origin - camera_offset
+
+func first_person_view() -> void:
+	transform.basis = target.global_transform.basis
+	transform.origin = target.global_transform.origin + Vector3.UP * 0.5
